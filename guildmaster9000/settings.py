b1f12826cccaf5930f9131570d53412e1a0b6dc5
Denis Mraovic <denis.mraovic@gmail.com>
2019-12-29 05:04:24 -0800

    simple updates

--- guildmaster9000/settings.py
+++ guildmaster9000/settings.py
@@ -140,7 +140,7 @@ USE_I18N = True
 
 USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 DEFAULT_CHARSET = 'utf-8'
 
--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
 from django.http import HttpResponse
 from django.utils.timezone import datetime
+
 
 from members.models import Member
 
@@ -20,3 +22,7 @@ def index(request):
 def member(request, member_id):
     member = get_object_or_404(Member, pk=member_id)
     return HttpResponse(member.name)
+
+
+def decay_gp(request, percentage=10):
+    Member.objects.all().update(gp=F('gp') * (1 - percentage/100))
