2106ad0486a34548bda3f4ec98356a55a983b88d
Denis Mraovic <denis.mraovic@gmail.com>
2020-01-05 04:30:09 -0800

    fixed settings path

--- guildmaster9000/wsgi.py
+++ guildmaster9000/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guildmaster9000.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guildmaster9000.production')
 
 application = get_wsgi_application()
