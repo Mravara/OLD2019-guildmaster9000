6991241406b58dd64201074307c19cba0db63580
Denis Mraovic <denis.mraovic@gmail.com>
2023-06-02 10:27:43 +0200

    settings files!!

---
+++ guildmaster9000/development.py
@@ -0,0 +1,23 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    # django toolbar
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+if DEBUG:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
